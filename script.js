ac3208ee960007052e386bf28b4ee938cac4d667
Ali Abbas <48610822+alivault@users.noreply.github.com>
2023-07-20 20:47:29 -0700

    add start and end hue sliders

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 window.onload = () => {
   const colorPicker = document.querySelector('#color-picker');
   const hueSlider = document.querySelector('#hue-slider');
+  const startHueSlider = document.querySelector('#start-hue-slider');
+  const endHueSlider = document.querySelector('#end-hue-slider');
   const saturationSlider = document.querySelector('#saturation-slider');
   const lightnessSlider = document.querySelector('#lightness-slider');
   const numTilesSlider = document.querySelector('#num-tiles-slider');
   const colorPalette = document.querySelector('#color-palette');
+  const colorOutput = document.querySelector('#color-output');
+  const hueOutput = document.querySelector('#hue-output');
+  const saturationOutput = document.querySelector('#saturation-output');
+  const lightnessOutput = document.querySelector('#lightness-output');
+  const startHueOutput = document.querySelector('#start-hue-output');
+  const endHueOutput = document.querySelector('#end-hue-output');
+
+  let hueShift = 0;
+  let prevHueSliderValue = hueSlider.value;
+
+  const wrapHue = hue => ((hue % 360) + 360) % 360;
 
   const updatePalette = () => {
     colorPalette.innerHTML = '';
@@ -17,6 +30,10 @@ window.onload = () => {
     const [L, C, H] = color.oklch();
     const lightnessScale = parseFloat(lightnessSlider.value);
     const saturationScale = saturationSlider.value / 100;
+    let hueRange = wrapHue(endHueSlider.value - startHueSlider.value);
+    // Adjust the hueRange if it's more than 180
+    hueRange = hueRange > 180 ? 360 - hueRange : hueRange;
+    const hueStep = hueRange / numTilesSlider.value;
 
     for (let i = 0; i < numTilesSlider.value; i++) {
       const tile = document.createElement('div');
@@ -29,7 +46,8 @@ window.onload = () => {
       const l = (1 - (i * (1 - 0.1)) / numTilesSlider.value) * lightnessScale;
       const c = C;
       const s = saturationScale;
-      const tileColor = chroma.oklch(l, c, H).set('hsl.s', s);
+      const h = wrapHue(parseFloat(startHueSlider.value) + hueStep * i);
+      const tileColor = chroma.oklch(l, c, h).set('hsl.s', s).set('hsl.h', h);
       const contrastColor =
         chroma.contrast(tileColor, 'white') >
         chroma.contrast(tileColor, 'black')
@@ -50,6 +68,32 @@ window.onload = () => {
     }
   };
 
+  colorPicker.addEventListener('input', () => {
+    colorOutput.value = colorPicker.value;
+  });
+  hueSlider.addEventListener('input', () => {
+    hueOutput.value = hueSlider.value;
+  });
+  saturationSlider.addEventListener('input', () => {
+    saturationOutput.value = saturationSlider.value;
+  });
+  lightnessSlider.addEventListener('input', () => {
+    lightnessOutput.value = lightnessSlider.value;
+  });
+  startHueSlider.addEventListener('input', () => {
+    startHueOutput.value = startHueSlider.value;
+  });
+  endHueSlider.addEventListener('input', () => {
+    endHueOutput.value = endHueSlider.value;
+  });
+
+  colorPicker.dispatchEvent(new Event('input'));
+  hueSlider.dispatchEvent(new Event('input'));
+  saturationSlider.dispatchEvent(new Event('input'));
+  lightnessSlider.dispatchEvent(new Event('input'));
+  startHueSlider.dispatchEvent(new Event('input'));
+  endHueSlider.dispatchEvent(new Event('input'));
+
   saturationSlider.addEventListener('input', updatePalette);
   colorPicker.addEventListener('input', () => {
     const color = chroma(colorPicker.value);
@@ -57,11 +101,24 @@ window.onload = () => {
     const hue = Math.round(color.get('hsl.h'));
     saturationSlider.value = saturation;
     hueSlider.value = hue;
+    startHueSlider.value = hue;
+    endHueSlider.value = hue;
+    prevHueSliderValue = hueSlider.value;
     updatePalette();
   });
 
   lightnessSlider.addEventListener('input', updatePalette);
-  hueSlider.addEventListener('input', updatePalette);
+  hueSlider.addEventListener('input', () => {
+    hueShift = hueSlider.value - prevHueSliderValue;
+    startHueSlider.value = wrapHue(parseInt(startHueSlider.value) + hueShift);
+    endHueSlider.value = wrapHue(parseInt(endHueSlider.value) + hueShift);
+    prevHueSliderValue = hueSlider.value;
+    startHueOutput.value = startHueSlider.value;
+    endHueOutput.value = endHueSlider.value;
+    updatePalette();
+  });
+  startHueSlider.addEventListener('input', updatePalette);
+  endHueSlider.addEventListener('input', updatePalette);
   numTilesSlider.addEventListener('input', () => {
     updatePalette();
     document.querySelector('#num-tiles-value').innerText = numTilesSlider.value;
