80e0fd28ac243070c4aef9d7c9d28e2f12d03acb
Ali Abbas <48610822+alivault@users.noreply.github.com>
2023-07-20 17:38:58 -0700

    add custom palette length feature

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ window.onload = () => {
   const hueSlider = document.querySelector('#hue-slider');
   const saturationSlider = document.querySelector('#saturation-slider');
   const lightnessSlider = document.querySelector('#lightness-slider');
-  const darkPalette = document.querySelector('.color-row.dark');
-  const lightPalette = document.querySelector('.color-row.light');
+  const numTilesSlider = document.querySelector('#num-tiles-slider');
+  const colorPalette = document.querySelector('#color-palette');
 
   const updatePalette = () => {
+    colorPalette.innerHTML = '';
+
     let color = chroma(colorPicker.value)
       .set('hsl.s', saturationSlider.value / 100)
       .set('hsl.h', hueSlider.value);
@@ -16,35 +18,39 @@ window.onload = () => {
     const lightnessScale = parseFloat(lightnessSlider.value);
     const saturationScale = saturationSlider.value / 100;
 
-    const setDivColors = (palette, startL, endL) => {
-      Array.from(palette.children).forEach((div, i) => {
-        const l = (startL + (i * (endL - startL)) / 4) * lightnessScale;
-        const c = C;
-        const s = saturationScale;
-        const color = chroma.oklch(l, c, H).set('hsl.s', s);
-        const contrastColor =
-          chroma.contrast(color, 'white') > chroma.contrast(color, 'black')
-            ? 'white'
-            : 'black';
-        div.style.backgroundColor = color.css();
-        div.style.color = contrastColor;
+    for (let i = 0; i < numTilesSlider.value; i++) {
+      const tile = document.createElement('div');
+      tile.className = 'color-tile';
+      tile.innerHTML = `
+        <span class="color-contrast"></span>
+        <span class="color-hex"></span>
+      `;
 
-        const hexElement = div.querySelector('.color-hex');
-        const contrastElement = div.querySelector('.color-contrast');
+      const l = (1 - (i * (1 - 0.1)) / numTilesSlider.value) * lightnessScale;
+      const c = C;
+      const s = saturationScale;
+      const tileColor = chroma.oklch(l, c, H).set('hsl.s', s);
+      const contrastColor =
+        chroma.contrast(tileColor, 'white') >
+        chroma.contrast(tileColor, 'black')
+          ? 'white'
+          : 'black';
+      tile.style.backgroundColor = tileColor.css();
+      tile.style.color = contrastColor;
 
-        hexElement.textContent = color.hex();
-        contrastElement.textContent = `${chroma
-          .contrast(color, contrastColor)
-          .toFixed(1)}`;
-      });
-    };
+      const hexElement = tile.querySelector('.color-hex');
+      const contrastElement = tile.querySelector('.color-contrast');
 
-    setDivColors(darkPalette, 0.1, 0.4);
-    setDivColors(lightPalette, 0.7, 1);
+      hexElement.textContent = tileColor.hex().substring(1);
+      contrastElement.textContent = `${chroma
+        .contrast(tileColor, contrastColor)
+        .toFixed(1)}`;
+
+      colorPalette.appendChild(tile);
+    }
   };
 
   saturationSlider.addEventListener('input', updatePalette);
-
   colorPicker.addEventListener('input', () => {
     const color = chroma(colorPicker.value);
     const saturation = Math.round(color.get('hsl.s') * 100);
@@ -56,5 +62,11 @@ window.onload = () => {
 
   lightnessSlider.addEventListener('input', updatePalette);
   hueSlider.addEventListener('input', updatePalette);
+  numTilesSlider.addEventListener('input', () => {
+    updatePalette();
+    document.querySelector('#num-tiles-value').innerText = numTilesSlider.value;
+  });
+
   updatePalette();
+  document.querySelector('#num-tiles-value').innerText = numTilesSlider.value;
 };
