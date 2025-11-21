be74008d99e5d2f0e95a639eb390e14efcc851cd
Ali Abbas <48610822+alivault@users.noreply.github.com>
2025-11-21 11:00:43 -0800

    setup cloudflare

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,51 @@
-import { defineConfig } from 'vite'
-import { devtools } from '@tanstack/devtools-vite'
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import viteReact from '@vitejs/plugin-react'
-import viteTsConfigPaths from 'vite-tsconfig-paths'
-import tailwindcss from '@tailwindcss/vite'
-import { nitro } from 'nitro/vite'
+import { defineConfig } from "vite";
+import { cloudflare } from "@cloudflare/vite-plugin";
+import { devtools } from "@tanstack/devtools-vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact from "@vitejs/plugin-react";
+import viteTsConfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
+import { nitro } from "nitro/vite";
+
+const WORKER_ENTRY = "virtual:tanstack-start-server-entry";
+
+const workerIndexChunkPlugin = () => ({
+  name: "worker-index-entry",
+  config() {
+    return {
+      environments: {
+        ssr: {
+          build: {
+            rollupOptions: {
+              input: {
+                index: WORKER_ENTRY,
+              },
+            },
+          },
+        },
+      },
+    };
+  },
+});
 
 const config = defineConfig({
   plugins: [
+    cloudflare({ viteEnvironment: { name: "ssr" } }),
     devtools(),
     nitro(),
     // this is the plugin that enables path aliases
     viteTsConfigPaths({
-      projects: ['./tsconfig.json'],
+      projects: ["./tsconfig.json"],
     }),
     tailwindcss(),
     tanstackStart(),
     viteReact({
       babel: {
-        plugins: [['babel-plugin-react-compiler', {}]],
+        plugins: [["babel-plugin-react-compiler", {}]],
       },
     }),
+    workerIndexChunkPlugin(),
   ],
-})
+});
 
-export default config
+export default config;
